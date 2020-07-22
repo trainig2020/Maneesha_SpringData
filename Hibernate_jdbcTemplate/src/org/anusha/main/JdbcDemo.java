8e7ec38f5db06f8f9c648a3d7942a692eb07d889
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-22 23:51:21 +0530

    Add files via upload

---
+++ Hibernate_jdbcTemplate/src/org/anusha/main/JdbcDemo.java
@@ -0,0 +1,28 @@
+package org.anusha.main;
+
+import org.anusha.dao.HibernateDaoImpl;
+import org.anusha.dao.JdbcDaoImpl;
+import org.anusha.dao.SimpleJdbcDaoImpl;
+import org.anusha.model.Circle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JdbcDemo {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		//JdbcDaoImpl dao = ctx.getBean("jdbcDaoImpl", JdbcDaoImpl.class);
+
+		//Circle circle = dao.getCircle(1);
+		//System.out.println(circle.getName());
+        //System.out.println(dao.getCircleCount());
+		HibernateDaoImpl dao1 = ctx.getBean("hibernateDaoImpl", HibernateDaoImpl.class);
+		
+		System.out.println(dao1.getCircleCount());
+		//System.out.println(dao.getAllCircle().size());
+		
+		//dao.createTriangleTable( );
+
+		
+	}
+}
+
